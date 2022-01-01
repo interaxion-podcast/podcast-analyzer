6ab1573f6a28a790284357cc62f9cf259929581c
oka <pn11@users.noreply.github.com>
2022-01-01 17:10:35 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Podcast Feed Analyzer
+
+![](out/fig_ep.png)
+![](out/fig_dur.png)
+
+## Usage
+
+```sh
+pipenv install
+pipenv run python run.py
+```
---
+++ run.py
@@ -0,0 +1,57 @@
+import urllib.request
+from datetime import date
+
+import pandas as pd
+import plotly.express as px
+import podcastparser
+import ruamel.yaml
+
+
+def parse(feed):
+    parsed = podcastparser.parse(feed, urllib.request.urlopen(feed))
+    episodes = parsed['episodes']
+    return sorted(episodes, key=lambda x: x['published'])
+
+
+def get_date_and_duration(name, episodes) -> pd.DataFrame:
+    dic = {'date': [], 'duration': [], 'name': [], 'one': []}
+    for e in episodes:
+        dic['date'].append(date.fromtimestamp(e['published']))
+        dic['duration'].append(e['total_time'])
+        dic['one'].append(1)
+        dic['name'].append(name)
+    df = pd.DataFrame.from_dict(dic)
+    return df
+
+
+def create_cdf(df):
+    fig_dur = px.ecdf(df, x='date', y='duration', color='name',
+                      ecdfnorm=None, labels={'duration': 'エピソード時間'})
+    fig_ep = px.ecdf(df, x='date', y='one', color='name',
+                     ecdfnorm=None, labels={'one': 'エピソード数'})
+    fig_dur.write_image('out/fig_dur.png')
+    fig_ep.write_image('out/fig_ep.png')
+    # fig_dur.show()
+    # fig_ep.show()
+
+
+def main():
+    yaml = ruamel.yaml.YAML()
+    with open('shows.yml') as f:
+        conf = yaml.load(f)
+    print(conf)
+    df = None
+    for c in conf:
+        print(c['name'], c['feed'])
+        if c['feed']:
+            episodes = parse(c['feed'])
+            if df is None:
+                df = get_date_and_duration(c['name'], episodes)
+            else:
+                df = pd.concat(
+                    [df, get_date_and_duration(c['name'], episodes)])
+    create_cdf(df)
+
+
+if __name__ == '__main__':
+    main()
